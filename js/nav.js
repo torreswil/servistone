31684205c3541b543dbeac3e3a33535b63c77cd3
Wilfredo Torres Ariza <wtorresariza@gmail.com>
2016-06-06 19:29:03 -0500

    Initial commit

---
+++ js/nav.js
@@ -0,0 +1,34 @@
+(function ($) {
+    
+    // Sticky Header
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 120) {
+            $(".srvs-hero-h").addClass('srvs-nav-h');
+        } else {
+            $(".srvs-hero-h").removeClass('srvs-nav-h');
+        }
+    });
+
+    // Mobile Navigation
+    $('.mobile-toggle').click(function() {
+        $('.srvs-hero-h').toggleClass('open-nav');
+    });
+
+    $('.srvs-hero-h li a').click(function() {
+        if ($('.srvs-hero-h').hasClass('open-nav')) {
+            $('.navigation').removeClass('open-nav');
+            $('.srvs-hero-h').removeClass('open-nav');
+        }
+    });
+
+    // Navigation Scroll - ljepo radi materem
+    $('nav a').click(function(event) {
+        var id = $(this).attr("href");
+        var offset = 70;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: target
+        }, 500);
+        event.preventDefault();
+    });
+})(jQuery);
