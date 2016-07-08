c03f9815b64712865b40b45439f1e10dee2b5df6
Wilfredo Torres Ariza <wtorresariza@gmail.com>
2016-07-07 21:05:07 -0500

    portfolio filters

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,13 @@
             $(".srvs-hero-h").removeClass('srvs-nav-h');
         }
     });
+    $(window).load(function() {
+        if ($(window).scrollTop() > 100) {
+            $(".srvs-hero-h").addClass('srvs-nav-h');
+        } else {
+            $(".srvs-hero-h").removeClass('srvs-nav-h');
+        }
+    });
 
     // Mobile Navigation
     $('.mobile-toggle').click(function() {
